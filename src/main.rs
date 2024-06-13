abc2dae0434e546670aef9bf7ddb711bb256c7fa
uhidontkno <hi@rare1k.dev>
2024-06-12 21:05:52 -0400

    feat: get file i/o working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 fn main() {
     println!("Hello, world!");
     println!("{}",1+4);
     println!("{}",9*10);
     println!("{}",16/2);
-    let stdout = stdout();
-    let message = String::from("hi chat");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    let path = Path::new("test.txt");
+    let display = path.display();
+    let message = "Hello, World!";
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+    match file.write_all(message.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
 }
