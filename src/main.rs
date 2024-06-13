5cdf38c4e1d1c1708e254790ce5204e58ffeadd7
uhidontkno <hi@rare1k.dev>
2024-06-12 20:31:21 -0400

    feat: learn how to use ferris says

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     println!("{}",1+4);
     println!("{}",9*10);
     println!("{}",16/2);
+    let stdout = stdout();
+    let message = String::from("hi chat");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
